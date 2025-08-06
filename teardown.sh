2e19563484ca2dcd02a4837e7b42bc448fd8877d
Daniel Garlick <deanbgarlick@gmail.com>
2025-08-06 12:05:05 -0700

    Add auto-approve to teardown

--- teardown.sh
+++ teardown.sh
@@ -30,28 +30,21 @@ export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/$KEY_FILE"
 log_message "Planning resource destruction..."
 terraform plan -destroy
 
-# Prompt for confirmation
-read -p "Are you sure you want to destroy all resources? This cannot be undone! (y/n) " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    log_message "Destroying all resources..."
-    terraform destroy -auto-approve
-    
-    # Clean up local files
-    log_message "Cleaning up local Terraform files..."
-    rm -rf .terraform* terraform.tfstate*
-    
-    log_message "Teardown complete! All resources have been destroyed."
-    
-    # Remind about service account cleanup
-    log_message "
+log_message "Destroying all resources..."
+terraform destroy -auto-approve
+
+# Clean up local files
+log_message "Cleaning up local Terraform files..."
+rm -rf .terraform* terraform.tfstate*
+
+log_message "Teardown complete! All resources have been destroyed."
+
+# Remind about service account cleanup
+log_message "
 Note: The Terraform deployer service account and its permissions still exist in GCP.
 If you want to remove these as well, you can:
 1. Delete the service account: gcloud iam service-accounts delete terraform-deployer@\$(gcloud config get-value project).iam.gserviceaccount.com
 2. Delete the key file: rm $KEY_FILE
 
 Or keep them if you plan to deploy again later.
-"
-else
-    log_message "Teardown cancelled"
-fi 
+" 
